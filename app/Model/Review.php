11753e0662d5025ddc5dbb7228b33be4c39ce729
riyanriyans <muhammadriyansetiawan@yahoo.co.id>
2018-01-03 06:23:52 +0700

    Product Resource Created

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
