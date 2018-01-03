01e1f7908db42386f8f8fa6902564bef1c3dbaa8
riyanriyans <muhammadriyansetiawan@yahoo.co.id>
2018-01-03 13:26:37 +0700

    Product not belongs to user

--- app/Exceptions/ExceptionTrait.php
+++ app/Exceptions/ExceptionTrait.php
@@ -20,7 +20,7 @@ trait ExceptionTrait
             return $this->HttpResponse($e);
         }
 
-        return parent::render($request, $exception);
+        return parent::render($request, $e);
     }
 
     protected function isModel($e)
