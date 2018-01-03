347717849605a47225dbfaca21109dcfda033d15
riyanriyans <muhammadriyansetiawan@yahoo.co.id>
2018-01-03 07:12:03 +0700

    Review Resource & Product Pagination

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return ProductCollection::collection(Product::all());
+        return ProductCollection::collection(Product::paginate(20));
         // return ProductResource::collection(Product::all());
     }
 
